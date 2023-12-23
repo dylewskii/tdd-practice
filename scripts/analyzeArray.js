99851114d5c3e717513ce2ec4ebc1cf9b5c0950a
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-23 22:47:51 +0100

    add analyzeArray script & test

--- scripts/analyzeArray.js
+++ scripts/analyzeArray.js
@@ -1,11 +1,32 @@
 const analyzeArray = (arr) => {
+    const findAvg = (foo) => {
+        const sum = foo.reduce((acc, curr) => acc + curr, 0);
+        const length = foo.length;
+        return sum / length;
+    };
 
+    const findMin = (foo) => {
+        return Math.min(...foo);
+    };
+
+    const findMax = (foo) => {
+        return Math.max(...foo);
+    };
+
+    const findLength = (foo) => {
+        return foo.length;
+    };
+
+    const avg = findAvg(arr);
+    const min = findMin(arr);
+    const max = findMax(arr);
+    const length = findLength(arr);
 
     return {
         average: avg,
-        min: minimun,
-        max: maximum,
-        length: len
+        min: min,
+        max: max,
+        length: length
     }
 }
 
