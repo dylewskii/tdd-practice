266bba1c962dbdf645e7d60207b629e997724e27
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-23 16:35:16 +0100

    add calculator object & tests

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,8 @@
-const calculator = {};
+const calculator = {
+    add : (a, b) => a + b,
+    subtract : (a, b) => a - b,
+    multiply : (a, b) => a * b,
+    divide : (a, b) => a / b
+};
 
 module.exports = calculator;
--- scripts/tests/calculator.test.js
+++ scripts/tests/calculator.test.js
@@ -1,1 +1,13 @@
 import calculator from "../calculator";
+
+test(("respetive func should take two numbers and return the correct calculation"), () => {
+    const addition = calculator.add(1, 1); // 2
+    const subtraction = calculator.subtract(2, 1); // 1
+    const division = calculator.divide(10, 2); // 5
+    const multiplication = calculator.multiply(5, 5); // 25
+
+    expect(addition).toBe(2);
+    expect(subtraction).toBe(1);
+    expect(division).toBe(5);
+    expect(multiplication).toBe(25);
+})
