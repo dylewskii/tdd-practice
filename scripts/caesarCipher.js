10400796083f75dd042ad474c8d4bac74b9afe69
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-23 22:22:45 +0100

    add caesar cipher script & test

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,5 +1,82 @@
-const caesarCipher = (str, shift) => {
+const caesarCipher = (str, shift=0) => {
+    const arrayToEncrypt = [...str];
+    const alphabetMix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    const higherIndexes = [];
 
-}
+    // Returns array of each letter's current alphabet position 
+    const findPositions = function (originalArr) {
+        const currentPositions = [];
+
+        originalArr.forEach((letter, i) => {
+            // spaces
+            if (letter === " "){
+                currentPositions.push(" ");
+            // non-alphabetical characters
+            } else if (!alphabetMix.includes(letter)){
+                currentPositions.push(letter);
+            // alphabetical characters
+            } else if (alphabetMix.includes(letter)){
+                // remember which letters are uppercase
+                if (letter === letter.toUpperCase()) {
+                    currentPositions.push(upperAlphabet.indexOf(letter));
+                    higherIndexes.push(i);
+                // push all lowercase letters normally
+                } else {
+                    currentPositions.push(lowerAlphabet.indexOf(letter));
+                }
+            } 
+        })
+        
+        return currentPositions;
+    }
+
+    // Returns shifted letter positions
+    const shiftPositions = function (positionsArr, shiftValue) {
+        const shiftedPositions = [];
+
+        positionsArr.forEach((pos) => {
+            let newPos = pos + shiftValue;
+            if (pos === " " || isNaN(pos)) {
+                shiftedPositions.push(pos);
+            } else if (newPos > 26){
+                const overlap = newPos % 26;
+                shiftedPositions.push(overlap);
+            } else {
+                shiftedPositions.push(newPos);
+            }
+        })
+
+        return shiftedPositions;
+    }
+
+    // Returns encrypted array values
+    const encryptValues = function (shiftedArr) {
+        const encrypted = [];
+
+        shiftedArr.forEach((value) => {
+            // spaces
+            if (value === " " || isNaN(value)){
+                encrypted.push(value);
+            // alphabetical characters
+            } else {
+                encrypted.push(lowerAlphabet[value]);
+            }
+        })
+
+        return encrypted;
+    }
+
+    const current = findPositions(arrayToEncrypt);
+    const shifted = shiftPositions(current, shift);
+    const ciphered = encryptValues(shifted);
+
+    higherIndexes.forEach((index) => {
+        ciphered[index] = ciphered[index].toUpperCase();
+    })
+    
+    return ciphered.join("");
+};
 
 module.exports = caesarCipher;
--- scripts/tests/caesarCipher.test.js
+++ scripts/tests/caesarCipher.test.js
@@ -1,1 +1,21 @@
 import caesarCipher from "../caesarCipher";
+
+test(("return correctly 'shifted' string, using shift value provided"), () => {
+        const testOne = caesarCipher("hello world", 3) // khoor zruog
+        expect(testOne).toBe("khoor zruog");
+})
+
+test(("return 'shifted' string, including uppercase letters"), () => {
+        const testTwo = caesarCipher("HELLO WORLD", 3) // KHOOR ZRUOG
+        expect(testTwo).toBe("KHOOR ZRUOG");
+})
+
+test(("return 'shifted' string, including punctuation"), () => {
+        const testThree = caesarCipher("!Hello World!", 3) // !Khoor Zruog!
+        expect(testThree).toBe("!Khoor Zruog!");
+})
+
+test(("return the 'shifted' string, ensuring to wrap from z to a"), () => {
+        const testFour = caesarCipher("Go to the zoo", 3) // Jr wr wkh crr
+        expect(testFour).toBe("Jr wr wkh crr");
+})
